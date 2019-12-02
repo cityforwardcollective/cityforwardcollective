4c8bfaf208f52113a53b95da5b9533c65565bad6
Spencer <spencer.schien@gmail.com>
2019-12-02 13:30:40 -0600

    adding database tables as objects to package

--- R/school_db_tables.R
+++ R/school_db_tables.R
@@ -1,1 +1,4 @@
 schools <- readRDS("data/schools.rds")
+enrollment <- readRDS("data/enrollment.rds")
+report_cards <- readRDS("data/report_cards.rds")
+forward_exam <- readRDS("data/forward_exam.rds")
