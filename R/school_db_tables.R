cb15810b0fffb02c59e503c7144d5f304370d052
Spencer <spencer.schien@gmail.com>
2019-12-03 16:35:52 -0600

    Deleting data files

--- R/school_db_tables.R
+++
@@ -1,4 +0,0 @@
-schools <- readRDS("data/schools.rds")
-enrollment <- readRDS("data/enrollment.rds")
-report_cards <- readRDS("data/report_cards.rds")
-forward_exam <- readRDS("data/forward_exam.rds")
