a69a37acf51595899b5260c6113ea957d68c5eb4
Spencer Schien <spencer.schien@cityforwardcollective.org>
2022-12-08 11:54:07 -0600

    updated ggplot theme

--- R/cfc_gg_theme.R
+++ R/cfc_gg_theme.R
@@ -7,6 +7,16 @@
 #' @export cfc_babyblue
 #' @export cfc_skyblue
 #' @export cfc_colors
+
+# ensure branded fonts are available
+.onload <- function(libname, pkgname) {
+  if (.Platform$OS.type == "windows") {
+    windowsFonts(Georgia = windowsFont("Georgia"),
+                 Verdana = windowsFont("Verdana"))
+  }
+}
+
+
 # Define Brand Colors
 
 cfc_darkblue <- "#003349"
@@ -29,9 +39,20 @@ ggplot <- function(...) {
   ggplot2::ggplot(...) +
     scale_fill_manual(values = cfc_colors, labels = function(x) str_wrap(x, 20)) +
     scale_color_manual(values = cfc_colors, labels = function(x) str_wrap(x, 20)) +
-    theme_minimal(base_family = "serif") +
-    theme(text = element_text(size = 14, family = "serif"),
+    theme_minimal() +
+    theme(text = element_text(family = "Verdana",
+                              size = 16),
           plot.title.position = "plot",
+          plot.title = element_text(family = "Georgia",
+                                    face = "bold",
+                                    size = 24,
+                                    margin = margin(b = 20)),
+          plot.subtitle = element_text(color = "grey30",
+                                       margin = margin(b = 20)),
           plot.caption.position = "plot",
-          plot.caption = element_text(hjust = 0))
+          plot.caption = element_text(hjust = 0,
+                                      lineheight = 1.2,
+                                      color = "grey50"),
+          panel.grid.minor = element_blank(),
+          panel.grid.major.x = element_blank())
 }
