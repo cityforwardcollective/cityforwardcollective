a7dc80b25d00b258d021298d3386e0770f9ec4bf
Spencer Schien <spencer.schien@cityforwardcollective.org>
2022-12-08 12:16:59 -0600

    updated run

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,7 @@
 # ensure branded fonts are available
-.onload <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) {
   if (.Platform$OS.type == "windows") {
     windowsFonts(Georgia = windowsFont("Georgia"),
                  Verdana = windowsFont("Verdana"))
   }
-
-  rlang::run_on_load()
 }
