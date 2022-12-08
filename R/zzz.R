c4ed071f83962a6c769b12310d28b4b9dae925ad
Spencer Schien <spencer.schien@cityforwardcollective.org>
2022-12-08 15:06:41 -0600

    added update_geom_defaults

--- R/zzz.R
+++ R/zzz.R
@@ -4,4 +4,8 @@
     windowsFonts(Georgia = windowsFont("Georgia"),
                  Verdana = windowsFont("Verdana"))
   }
+
+  update_geom_defaults(GeomPoint, list(colour = cfc_darkblue))
+  update_geom_defaults(GeomRect, list(fill = cfc_darkblue))
+  update_geom_defaults(GeomPath, list(color = cfc_darkblue))
 }
