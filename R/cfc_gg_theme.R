aa6005e6ce897e733820b75ec83e82d084eb52ad
Spencer Schien <spencer.schien@gmail.com>
2026-02-12 19:30:46 -0600

    updated brand colors

--- R/cfc_gg_theme.R
+++ R/cfc_gg_theme.R
@@ -2,25 +2,47 @@
 #' @export ggplot
 #' @export cfc_darkblue
 #' @export cfc_orange
-#' @export cfc_teal
+#' @export cfc_turquoise
 #' @export cfc_babyblue
 #' @export cfc_skyblue
 #' @export cfc_colors
+#' @export cfc_dark_green
+#' @export cfc_orange
+#' @export cfc_stone
+#' @export cfc_teal
+#' @export cfc_sector_cols
 
 
 # Define Brand Colors
+# These are old now
 
 cfc_darkblue <- "#003349"
 cfc_orange <- "#E57200"
-cfc_teal <- "#69DBC8"
+cfc_turquoise <- "#69DBC8"
 cfc_babyblue <- "#C7DBF4"
 cfc_skyblue <- "#00A9E0"
+
+# New Brand Colors
+
+cfc_dark_green <- "#395549"
+cfc_orange <- "#ed902d"
+cfc_stone <- "#6998ab"
+cfc_teal <- "#348789"
+
+# Define sector colors
+
+cfc_sector_cols <- c(
+  cfc_dark_green,
+  cfc_orange,
+  cfc_stone,
+  cfc_teal
+)
 
 # Define palette
 
 cfc_colors <- c(cfc_darkblue,
                 cfc_orange,
-                cfc_teal,
+                cfc_turquoise,
                 cfc_babyblue,
                 cfc_skyblue)
 
